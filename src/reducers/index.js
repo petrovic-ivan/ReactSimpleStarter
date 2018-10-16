1a48e4aad471b74bf4d9d219b895597821214239
Ivan Petrovic <ipetrovic@Ivans-MacBook-Air.local>
2018-10-15 22:55:52 -0700

    Replacing with new lessons.

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import BookReducer from './reducer_book';
+
+const rootReducer = combineReducers({
+    books: BookReducer
+});
+
+export default rootReducer;
