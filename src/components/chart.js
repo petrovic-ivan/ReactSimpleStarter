ec9b56b019cf217c29f41f003f086b6f67711754
Ivan Petrovic <ipetrovic@Ivans-MacBook-Air.local>
2018-10-20 14:46:29 -0700

    Added new features.

---
+++ src/components/chart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+
+export default (props) => {
+    return (
+        <div>
+            <Sparklines height={120} width={180} data={props.data}>
+                <SparklinesLine color={props.color}></SparklinesLine>
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+        </div>
+    );
+}
