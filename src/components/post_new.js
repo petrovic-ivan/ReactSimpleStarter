6b57a2c96e497f2be02910a3f13612c570a2421d
Petrovic, Ivan <ivan.petrovic@moodys.com>
2018-10-25 17:32:13 -0700

    Adde navigation.

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostNew extends Component {
     
@@ -20,7 +22,10 @@ class PostNew extends Component {
     }
 
     onSubmit(values) {
-        console.log(values);
+        this.props.createPost(values, () => {
+            debugger;
+            this.props.history.push('/');
+        });
     }
 
     render() {
@@ -70,4 +75,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostNew);
+})(
+    connect(null, { createPost })(PostNew)
+);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,13 +1,13 @@
 import { FETCH_POSTS } from '../actions';
 
-export default function(state = {}, action) {
-    switch(action.type) {
+export default function (state = {}, action) {
+    switch (action.type) {
         case FETCH_POSTS:
             const posts = {};
             action.payload.data.forEach(element => {
                 posts[element.id] = element;
             });
-        return posts;
+            return posts;
         default:
             return state;
     }
